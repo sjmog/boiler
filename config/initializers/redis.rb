b4e5997a13055633555b21fc8058b2a468ba9fd3
Sam Morgan <s_morgan@me.com>
2024-09-25 17:42:22 +0100

    Fixup test step in CI workflow
    Also remove the linting step

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,12 +1,6 @@
 require "uri"
 
-if ENV["REDIS_URL"]
-  url = URI.parse(ENV["REDIS_URL"])
-  url.scheme = "rediss"
-  url.port = Integer(url.port) + 1
-  $redis = Redis.new(url: url, driver: :ruby, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
-else
-  # Use a dummy Redis configuration or skip Redis initialization
-  puts "REDIS_URL not set. Skipping Redis initialization."
-  $redis = nil
-end
+url = URI.parse(ENV.fetch("REDIS_URL") { "redis://127.0.0.1:6379/12" })
+url.scheme = "rediss"
+url.port = Integer(url.port) + 1
+$redis = Redis.new(url: url, driver: :ruby, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
